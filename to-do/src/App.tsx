c1be7375ff3e0019b3831b170ca2bfb3c76aba95
Kevin Feliz Henriquez <kfh.tech.programmer@gmail.com>
2023-06-22 11:53:55 -1100

    Adding the first functional files.

--- to-do/src/App.tsx
+++ to-do/src/App.tsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Todos } from './components/Todos'
 
-function App() {
-  const [count, setCount] = useState(0)
+const mockTodos = [
+  {
+    id: '1',
+    title: 'Iniciar el proyecto todo con react y typescript',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'hacer la tarea de la universidad',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'tomar los examenes finales de la universidad',
+    completed: false
+  }
+]
 
+const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='todoapp'>
+        <Todos todos={todos} />
+    </div>
     </>
   )
 }
